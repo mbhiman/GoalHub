44eaf4aa1f1786c09decbdcc13faf8a3b2c94d21
Bhiman Singha <mbhiman70@gmail.com>
2024-10-29 22:06:14 +0530

    Make some APIs

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const { createGoal, updateGoal } = require('./types');
 const app = express();
 
 app.use(express.json());
 
 app.post("/goal", (req, res) => {
-
+    const createPlayload = req.body;
+    const parsedplayload = createGoal.safeParse(createPlayload);
+    if(!parsedplayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    
 
 });
 
@@ -13,7 +22,15 @@ app.get("/goals", (req, res) => {
 });
 
 app.put("/completed", (req, res) => {
-
+    const updatePayload = req.body;
+    const parsedplayload = updateGoal.safeParse(updatePayload);
+    if(!parsedplayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    
 });
 
 
