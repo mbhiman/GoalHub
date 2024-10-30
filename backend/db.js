5c32e25b52a255737cb79633fd47dd44e990dce3
Bhiman Singha <mbhiman70@gmail.com>
2024-10-30 19:49:02 +0530

    Updated the APIs and goal schema

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://mbhiman70:hyvsITdwLwXjNFKf@bondhub-cluster0.u5lza.mongodb.net/GoalHub");
 
-const goalSchema = mongoose.Schema({
+const goalSchema = new mongoose.Schema({
     title: String,
     description: String,
     completed: Boolean
 })
 
-const goal = mongoose.model('goals', goalSchema);
+const Goal = mongoose.model('Goal', goalSchema);
 
-module.exports = { goal };
+module.exports = Goal;
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { createGoal, updateGoal } = require('./types');
+const Goal = require('./db');
 const app = express();
 
 app.use(express.json());
 
-app.post("/goal", (req, res) => {
+app.post("/goal", async (req, res) => {
     const createPlayload = req.body;
     const parsedplayload = createGoal.safeParse(createPlayload);
     if (!parsedplayload.success) {
@@ -14,22 +15,44 @@ app.post("/goal", (req, res) => {
         return;
     }
 
+    await Goal.create({
+        title: createPlayload.title,
+        description: createPlayload.description,
+        completed: false,
+    })
+
+    res.json({
+        msg: "Goal created"
+    })
 
 });
 
-app.get("/goals", (req, res) => {
+app.get("/goals", async (req, res) => {
+    const goals = await Goal.find({});
 
+    res.json(
+        goals
+    )
 });
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
     const updatePayload = req.body;
     const parsedplayload = updateGoal.safeParse(updatePayload);
     if (!parsedplayload.success) {
-        res.status(411).json({
+        res.status(411).json({ 
             msg: "You sent the wrong inputs",
         })
         return;
     }
+
+    await Goal.update({
+        _id: req.body.id,
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Goal marked as completed"
+    })
 
 });
 
